746bb647c1655f9014c822e699fa02e0737f7762
vibo777 <54715728+vibo777@users.noreply.github.com>
2022-01-26 00:58:32 +0530

    update

--- model/video-model.js
+++ model/video-model.js
@@ -2,16 +2,19 @@ const mongoose = require('mongoose');
 
 const videoSchema = new mongoose.Schema({
 
-    name:{type:String,required:true},
-    genre:{type:String,required:true},
+    title:{type:String,required:true},
+    year:{type:Number,required:true},
+    genres:[{type:String}],
+    contentRating:{type:Number,required:true},
+    duration:{type:Number,required:true},    
     releaseDate:{type:String,required:true},
-    runTime: {type: String, required: true},
-    description: {type: String, required: true},
+    originalTitle:{type:String,required:true},
+    storyline:{type:String,required:true},
     actors: [{type: String}],
-    rating: {type: Number, min: 0, max: 10},
-    production: {type: String, required: true},
-    director: [{type: String}],
-    videoPath: {type: String, required: true},
+    imdbRating:{type:Number,min:1,max:10},
+    posteurl:{type:String,required:true},
+    videoPath:{type:String,required:true},
+
 },{timestamps:true});
 
 const videoModel = new mongoose.model('video',videoSchema);
